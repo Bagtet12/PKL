9284ca4b9a2c9d0ee963b8bfd6689dd080877250
Aldi Milansyah Putra <54979160+Aldimilansyahputra@users.noreply.github.com>
2021-02-09 15:16:06 +0700

    Update portfolio.blade.php

--- resources/views/page/portfolio.blade.php
+++ resources/views/page/portfolio.blade.php
@@ -1,25 +1,50 @@
-@extends('template.template')
-@section('portfolio')
-    <header class="mastport">
-      <h4>PORTOFOLIO</h4>
-      <h1>TIMES INDONESIA - BIRO JEMBER</h1>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Times Indonesia Biro Jember</title>
+        <link rel="icon" type="image/x-icon" href="assets/img/favicon.ico" />
+        <!-- Font Awesome icons (free version)-->
+        <script src="https://use.fontawesome.com/releases/v5.15.1/js/all.js" crossorigin="anonymous"></script>
+        <!-- Google fonts-->
+        <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css" />
+        <link href="https://fonts.googleapis.com/css?family=Droid+Serif:400,700,400italic,700italic" rel="stylesheet" type="text/css" />
+        <link href="https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700" rel="stylesheet" type="text/css" />
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <link href="css/styles.css" rel="stylesheet" />
+        <link href="css/portfolio.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">
+            <div class="container">
+                <a class="navbar-brand js-scroll-trigger" href="#page-top"><img src="assets/img/logo.png" alt="" /></a>
+                <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">Menu<i class="fas fa-bars ml-1"></i></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav text-uppercase ml-auto">
+                        <li class="nav-item"><a class="nav-link js-scroll-trigger" href="{{url("/")}}">Profile</a></li>
+                        <li class="nav-item"><a class="nav-link js-scroll-trigger" href="">Our Product</a></li>
+                        <li class="nav-item"><a class="nav-link js-scroll-trigger" href="">Portfolio</a></li>
+                        <li class="nav-item"><a class="nav-link js-scroll-trigger" href="https://www.instagram.com/timesjember/" target="blank"><i class="fab fa-instagram"></i></a></li>
+                        <li class="nav-item"><a class="nav-link js-scroll-trigger" href="https://www.youtube.com/channel/UCgj4fXpMKECuDIOTcx-CvLg" target="blank"><i class="fab fa-youtube"></i></a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- mastport-->
+        <header class="mastport">
+            <div class="container">
+                <div class="mastport-heading text-uppercase">PORTOFOLIO KAMI</div>
+                <!-- <div class="mastport-subheading"></div> -->
+            </div>
         </header>
 
-        <!-- Container for the image gallery -->
-
-
-        <!-- Team-->
-
-        <div class="jumbotron">
-            <h1>TIMES Jember</h1>
-            <p>Provide Bussiness need throughout this era of technology. We're working on various things like Web Development, Influencer Management, PR Consultant, and many more</p>
-        </div>
-
-        <section class="page-section bg-light" id="team">
             <div class="container">
                 <div class="text-center">
-                    <h2 class="section-heading text-uppercase">EVENT GALLERY</h2>
-                    <h3 class="section-subheading text-muted">___________________________________</h3>
+                    <h2 class="section-headingport text-uppercase">EVENT GALLERY</h2>
                 </div>
 
                 <!-- Slideshow container -->
@@ -87,19 +112,12 @@
                 <div class="container">
                   <p class="textport">
                     Menyongsong kebangkitan era informasi online, kami TIMES Indonesia Network (TIN) sebagai media online berjejaring terbesar se-Indonesia, hadir menjawab kebutuhan zaman. Kami hadir dengan semangat kuat dan mengusung misi BUILDING - INSPIRING - POSITIVE THINKING.
-
-
                   </p>
-
                 </div>
-
-
 
                 <div class="text-center">
-                    <h2 class="section-heading text-uppercase">Our Affiliation</h2>
-                    <h3 class="section-subheading text-muted">___________________________________</h3>
+                    <h2 class="section-headingporto text-uppercase">EVENT KAMI</h2>
                 </div>
-
 
                 <div class="row">
                     <div class="col-lg-4">
@@ -158,11 +176,20 @@
                         </div>
                     </div>
                 </div>
-                <div class="row">
-                    <div class="col-lg-8 mx-auto text-center"><p class="large text-muted">Quotes</p></div>
-                </div>
             </div>
-        </section> --}}
 
 
-@endsection
+
+
+        <!-- Bootstrap core JS-->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js"></script>
+        <!-- Third party plugin JS-->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.min.js"></script>
+        <!-- Contact form JS-->
+        <script src="assets/mail/jqBootstrapValidation.js"></script>
+        <script src="assets/mail/contact_me.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
