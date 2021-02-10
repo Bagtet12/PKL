7232482601e98d1e1ce587562273d0250f3f450b
Aldi Milansyah Putra <182410102028@students.unej.ac.id>
2021-02-10 16:24:52 +0700

    prod

---
+++ resources/views/page/ourproduct.blade.php
@@ -0,0 +1,217 @@
+@extends ('template.template')
+@section ('ourproduct')
+
+  <header class="masthead">
+          <div class="container">
+              <div class="masthead-heading " >OUR PRODUCT</div>
+          </div>
+      </header>
+
+      <div class="container" style="margin-top:50px; margin-bottom:30px">
+        <div class="text-center">
+            <h1 class="section-heading text-uppercase">Web Development</h1>
+            <h6 class="section-subheading text-muted">pengembangan website</h6>
+            <img src="assets/img/beauty-fest.jpg" alt="web dev" style="width:75%; margin-top:30px">
+            <p style="margin-top:40px">Menyongsong kebangkitan era informasi online, kami TIMES Indonesia Network (TIN) sebagai media online berjejaring terbesar se-Indonesia, hadir menjawabkebutuhan zaman. Kami hadir dengan semangat kuat dan mengusung misi BUILDING - INSPIRING - POSITIVE THINKING. TIMES Indonesia Network (TIN) hadir tidak hanya dengan konsep informasi global namun juga dengan informasi regional dan lokal. Oleh karenanya, dengan tetap menyajikan informasi global melalui TIN, konsep berita regional dan lokal</p>
+            <a class="btn btn-dark btn-social mx-2" href="#!"><i class="fab fa-twitter"></i></a>
+            <a class="btn btn-dark btn-social mx-2" href="#!"><i class="fab fa-facebook-f"></i></a>
+        </div>
+      </div>
+      <div class="container">
+        <ul class="timeline">
+          <li>
+            <div class="timeline-panel">
+            </div>
+          </li>
+              <!-- <li class="timeline-inverted">
+                  <div class="timeline-inverted">
+
+                  </div>
+              </li> -->
+        </ul>
+      </div>
+
+
+
+      <div class="container" style="margin-top:50px">
+        <div class="text-center">
+            <h1 class="section-heading text-uppercase">PR Consultant</h1>
+            <h6 class="section-subheading text-muted">pengembangan website</h6>
+            <img src="assets/img/beauty-fest.jpg" alt="web dev" style="width:75%; margin-top:30px">
+            <p style="margin-top:40px">Menyongsong kebangkitan era informasi online, kami TIMES Indonesia Network (TIN) sebagai media online berjejaring terbesar se-Indonesia, hadir menjawabkebutuhan zaman. Kami hadir dengan semangat kuat dan mengusung misi BUILDING - INSPIRING - POSITIVE THINKING. TIMES Indonesia Network (TIN) hadir tidak hanya dengan konsep informasi global namun juga dengan informasi regional dan lokal. Oleh karenanya, dengan tetap menyajikan informasi global melalui TIN, konsep berita regional dan lokal</p>
+        </div>
+      </div>
+      <div class="container">
+        <ul class="timeline">
+          <li>
+            <div class="timeline-panel">
+            </div>
+          </li>
+              <!-- <li class="timeline-inverted">
+                  <div class="timeline-inverted">
+
+                  </div>
+              </li> -->
+        </ul>
+        </div>
+      </div>
+
+
+      <div class="container" style="margin-top:50px">
+        <div class="text-center">
+            <h1 class="section-heading text-uppercase">Influencer Activation Management</h1>
+            <h6 class="section-subheading text-muted">pengembangan website</h6>
+            <img src="assets/img/beauty-fest.jpg" alt="web dev" style="width:75%; margin-top:30px">
+            <p style="margin-top:40px">Menyongsong kebangkitan era informasi online, kami TIMES Indonesia Network (TIN) sebagai media online berjejaring terbesar se-Indonesia, hadir menjawabkebutuhan zaman. Kami hadir dengan semangat kuat dan mengusung misi BUILDING - INSPIRING - POSITIVE THINKING. TIMES Indonesia Network (TIN) hadir tidak hanya dengan konsep informasi global namun juga dengan informasi regional dan lokal. Oleh karenanya, dengan tetap menyajikan informasi global melalui TIN, konsep berita regional dan lokal</p>
+        </div>
+      </div>
+      <div class="container">
+        <ul class="timeline">
+          <li>
+            <div class="timeline-panel">
+            </div>
+          </li>
+              <!-- <li class="timeline-inverted">
+                  <div class="timeline-inverted">
+
+                  </div>
+              </li> -->
+        </ul>
+        </div>
+      </div>
+
+
+      <div class="container" style="margin-top:50px">
+        <div class="text-center">
+            <h1 class="section-heading text-uppercase">Creative Video & Broadcasting</h1>
+            <h6 class="section-subheading text-muted">pengembangan website</h6>
+            <img src="assets/img/beauty-fest.jpg" alt="web dev" style="width:75%; margin-top:30px">
+            <p style="margin-top:40px">Menyongsong kebangkitan era informasi online, kami TIMES Indonesia Network (TIN) sebagai media online berjejaring terbesar se-Indonesia, hadir menjawabkebutuhan zaman. Kami hadir dengan semangat kuat dan mengusung misi BUILDING - INSPIRING - POSITIVE THINKING. TIMES Indonesia Network (TIN) hadir tidak hanya dengan konsep informasi global namun juga dengan informasi regional dan lokal. Oleh karenanya, dengan tetap menyajikan informasi global melalui TIN, konsep berita regional dan lokal</p>
+        </div>
+      </div>
+      <div class="container">
+        <ul class="timeline">
+          <li>
+            <div class="timeline-panel">
+            </div>
+          </li>
+              <!-- <li class="timeline-inverted">
+                  <div class="timeline-inverted">
+
+                  </div>
+              </li> -->
+        </ul>
+        </div>
+      </div>
+
+
+      <div class="container" style="margin-top:50px">
+        <div class="text-center">
+            <h1 class="section-heading text-uppercase">Content Management System</h1>
+            <h6 class="section-subheading text-muted">pengembangan website</h6>
+            <img src="assets/img/beauty-fest.jpg" alt="web dev" style="width:75%; margin-top:30px">
+            <p style="margin-top:40px">Menyongsong kebangkitan era informasi online, kami TIMES Indonesia Network (TIN) sebagai media online berjejaring terbesar se-Indonesia, hadir menjawabkebutuhan zaman. Kami hadir dengan semangat kuat dan mengusung misi BUILDING - INSPIRING - POSITIVE THINKING. TIMES Indonesia Network (TIN) hadir tidak hanya dengan konsep informasi global namun juga dengan informasi regional dan lokal. Oleh karenanya, dengan tetap menyajikan informasi global melalui TIN, konsep berita regional dan lokal</p>
+        </div>
+      </div>
+      <div class="container">
+        <ul class="timeline">
+          <li>
+            <div class="timeline-panel">
+            </div>
+          </li>
+              <!-- <li class="timeline-inverted">
+                  <div class="timeline-inverted">
+
+                  </div>
+              </li> -->
+        </ul>
+        </div>
+      </div>
+
+
+      <div class="container" style="margin-top:50px">
+        <div class="text-center">
+            <h1 class="section-heading text-uppercase">Event Management</h1>
+            <h6 class="section-subheading text-muted">pengembangan website</h6>
+            <img src="assets/img/beauty-fest.jpg" alt="web dev" style="width:75%; margin-top:30px">
+            <p style="margin-top:40px">Menyongsong kebangkitan era informasi online, kami TIMES Indonesia Network (TIN) sebagai media online berjejaring terbesar se-Indonesia, hadir menjawabkebutuhan zaman. Kami hadir dengan semangat kuat dan mengusung misi BUILDING - INSPIRING - POSITIVE THINKING. TIMES Indonesia Network (TIN) hadir tidak hanya dengan konsep informasi global namun juga dengan informasi regional dan lokal. Oleh karenanya, dengan tetap menyajikan informasi global melalui TIN, konsep berita regional dan lokal</p>
+        </div>
+      </div>
+      <div class="container">
+        <ul class="timeline">
+          <li>
+            <div class="timeline-panel">
+            </div>
+          </li>
+              <!-- <li class="timeline-inverted">
+                  <div class="timeline-inverted">
+
+                  </div>
+              </li> -->
+        </ul>
+        </div>
+      </div>
+
+
+
+      <!-- <div class="container" style="margin-top:30px">
+        <div class="row">
+          <div class="col-sm-4">
+            <h3>IDEA FEST</h3>
+            <h5>Lippo Plaza Jember</h5>
+            <p style="margin-top:40px">Menyongsong kebangkitan era informasi online, kami TIMES IndonesiaNetwork (TIN) sebagai media online berjejaring terbesar se-Indonesia, hadir menjawabkebutuhan zaman. Kami hadir dengan semangat kuat dan mengusung misi BUILDING - INSPIRING - POSITIVE THINKING. TIMES Indonesia Network (TIN) hadir tidak hanya dengan konsep informasi global namun juga dengan informasi regional dan lokal. Oleh karenanya, dengan tetap menyajikan informasi global melalui TIN, konsep berita regional dan lokal</p>
+            <hr class="d-sm-none">
+          </div>
+          <div class="col-sm-8" style="margin-top:30px">
+            <div class="card-img-top"><img class="rounded-square img-fluid" src="assets/img/beauty-fest.jpg" alt="" /></div>
+            <br>
+          </div>
+        </div>
+      </div>
+
+
+
+      <div class="container" style="margin-top:30px">
+        <div class="row">
+          <div class="col-sm-8">
+            <div class="card-img-top"><img class="rounded-square img-fluid" src="assets/img/beauty-fest.jpg" alt="" /></div>
+            <br>
+          </div>
+          <div class="col-sm-4" style="margin-top:30px">
+            <h2>TITLE HEAD</h2>
+            <h5>Sub Heading</h5>
+            <p style="margin-top:40px">Menyongsong kebangkitan era informasi online, kami TIMES IndonesiaNetwork (TIN) sebagai media online berjejaring terbesar se-Indonesia, hadir menjawabkebutuhan zaman. Kami hadir dengan semangat kuat dan mengusung misi BUILDING - INSPIRING - POSITIVE THINKING. TIMES Indonesia Network (TIN) hadir tidak hanya dengan konsep informasi global namun juga dengan informasi regional dan lokal. Oleh karenanya, dengan tetap menyajikan informasi global melalui TIN, konsep berita regional dan lokal</p>
+            <hr class="d-sm-none">
+          </div>
+        </div>
+      </div>
+
+      <div class="container" style="margin-top:30px">
+        <div class="row">
+          <div class="col-sm-4">
+            <h2>TITLE HEAD</h2>
+            <h5>Sub Heading</h5>
+            <p style="margin-top:40px">Menyongsong kebangkitan era informasi online, kami TIMES IndonesiaNetwork (TIN) sebagai media online berjejaring terbesar se-Indonesia, hadir menjawabkebutuhan zaman. Kami hadir dengan semangat kuat dan mengusung misi BUILDING - INSPIRING - POSITIVE THINKING. TIMES Indonesia Network (TIN) hadir tidak hanya dengan konsep informasi global namun juga dengan informasi regional dan lokal. Oleh karenanya, dengan tetap menyajikan informasi global melalui TIN, konsep berita regional dan lokal</p>
+            <hr class="d-sm-none" style="margin-top:30px">
+          </div>
+          <div class="col-sm-8">
+            <div class="card-img-top"><img class="rounded-square img-fluid" src="assets/img/beauty-fest.jpg" alt="" /></div>
+            <br>
+          </div>
+        </div>
+      </div>
+
+      <div class="container" style="margin-top:30px">
+        <div class="row">
+          <div class="col-sm-8">
+            <div class="card-img-top"><img class="rounded-square img-fluid" src="assets/img/beauty-fest.jpg" alt="" /></div>
+            <br>
+          </div>
+          <div class="col-sm-4" style="margin-top:30px">
+            <h2>TITLE HEAD</h2>
+            <h5>Sub Heading</h5>
+            <p style="margin-top:40px">Menyongsong kebangkitan era informasi online, kami TIMES IndonesiaNetwork (TIN) sebagai media online berjejaring terbesar se-Indonesia, hadir menjawabkebutuhan zaman. Kami hadir dengan semangat kuat dan mengusung misi BUILDING - INSPIRING - POSITIVE THINKING. TIMES Indonesia Network (TIN) hadir tidak hanya dengan konsep informasi global namun juga dengan informasi regional dan lokal. Oleh karenanya, dengan tetap menyajikan informasi global melalui TIN, konsep berita regional dan lokal</p>
+            <hr class="d-sm-none">
+          </div>
+        </div>
+      </div> -->
